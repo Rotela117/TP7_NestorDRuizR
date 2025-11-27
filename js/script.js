d0db4dfb0434f13a7a6015edf5c69eac6551298a
Rotela117 <Rotela117@hotmail.com>
2025-11-26 21:16:01 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.getElementById("menuBtn");
+    const navMenu = document.getElementById("navMenu");
+
+    menuBtn.addEventListener("click", () => {
+        navMenu.classList.toggle("show");
+    });
+});
